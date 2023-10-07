1970ff8d0ebe70df25430f85b2ac9bf04dc7913b
srinivasan0077 <srinivasandhandapani071201@gmail.com>
2023-10-07 20:06:00 +0530

    adjusted size in item box

--- src/Home/Box.js
+++ src/Home/Box.js
@@ -7,7 +7,6 @@ function BoxComponent(props){
         <div className="box-container" onClick={()=>{ navigate("/view-item/"+props.variant.variantId);}} >
              <img className="box-img" alt="box-img" src={props.variant.images[0].url}/>
              <div className="box-description">
-                <div className="font-style-item-price">{props.variant.item.productItemName}</div>
                 <div className="font-style-variant">{props.variant.name}</div>
                 <div className="font-style-item-price">Price:{props.variant.price}</div>
              </div>
